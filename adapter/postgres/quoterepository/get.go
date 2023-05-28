89e6f19fd24528f038dc9a863daa917b97b8b0aa
Vanessa <vanessa.nunez.p@gmail.com>
2023-05-28 17:53:07 -0300

    feat: create get function to return quotes

--- adapter/postgres/quoterepository/get.go
+++ adapter/postgres/quoterepository/get.go
@@ -7,6 +7,27 @@ import (
 	"github.com/vanessanunes/frete-rapido/core/domain"
 )
 
+func (conn *Connection) GetCarriers(idDispactcher string) (carriers []domain.Carrier, err error) {
+	query := `select c.name , o.service , o.final_price , o.delivery_time_days from carrier c 
+		inner join offer o on o.id = c.id_offer 
+		inner join dispatcher d on d.id = o.id_dispatcher 
+		where d.id = $1`
+	rows, err := conn.db.Query(query, idDispactcher)
+	if err != nil {
+		log.Println(err)
+		return carriers, err
+	}
+	var carrier domain.Carrier
+	for rows.Next() {
+		err = rows.Scan(&carrier.Name, &carrier.Service, &carrier.Price, &carrier.Deadline)
+		if err != nil {
+			log.Println(err)
+		}
+		carriers = append(carriers, carrier)
+	}
+	return
+}
+
 func (conn *Connection) GetMetrics(lastQuote int) (metrics []domain.Metrics, err error) {
 	query := `select c.name, count(c.name) as quantity, avg(o.final_price) as price_avg, min(o.final_price) as min_price, max(o.final_price) as max_price
 	from offer o
